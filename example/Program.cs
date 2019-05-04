b3a6560fcb20a4c7300c9aa34c6001126c39eb03
Bastian Blokland <bastianblokland@gmail.com>
2019-05-04 12:53:37 +0300

    Add example project.
    Showing how to setup prebuild tasks for generating
    the enums.

---
+++ example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Months:");
+            foreach (var month in Enum.GetNames(typeof(Months)))
+                Console.WriteLine($"- {month}");
+
+            Console.WriteLine("\nExpensive products:");
+            foreach (var productName in Enum.GetNames(typeof(ExpensiveProducts)))
+                Console.WriteLine($"- {productName}");
+        }
+    }
+}
---
+++ example/readme.md
@@ -0,0 +1,3 @@
+﻿# **Enum-Generator** Example.
+
+This example shows setting up a pre-build task for generating a enums based on json data-sets.
