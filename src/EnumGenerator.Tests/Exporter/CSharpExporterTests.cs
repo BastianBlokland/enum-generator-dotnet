6e52a8186d3e1a57b815ff5435e3a86102b75f32
Bastian Blokland <bastianblokland@gmail.com>
2019-08-07 17:47:58 +0300

    Add 'HeaderMode' for controlling the export header.

--- src/EnumGenerator.Tests/Exporter/CSharpExporterTests.cs
+++ src/EnumGenerator.Tests/Exporter/CSharpExporterTests.cs
@@ -287,5 +287,30 @@ namespace Test {{
 ",
                 actual: export);
         }
+
+        [Fact]
+        public void CanBeExportedWithoutHeader()
+        {
+            var builder = new EnumBuilder("TestEnum");
+            builder.PushEntry("A", 1);
+            builder.PushEntry("B", 2);
+            var enumDef = builder.Build();
+
+            var export = enumDef.ExportCSharp(headerMode: HeaderMode.None);
+
+            Assert.Equal(
+                expected:
+$@"using System.CodeDom.Compiler;
+
+[GeneratedCode(""EnumGenerator.Core"", ""{Version}"")]
+public enum TestEnum
+{{
+    A = 1,
+
+    B = 2,
+}}
+",
+                actual: export);
+        }
     }
 }
